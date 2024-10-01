113c035b6c31aa47995e91c6e709bb5851e232e9
arksohail <arksohail@gmail.com>
2024-10-01 21:44:23 +0530

    Extracting Dynamic Route Parameters via Observables

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,5 +1,7 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, OnInit, signal } from '@angular/core';
 import { UsersService } from '../users.service';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../user/user.model';
 
 @Component({
   selector: 'app-user-tasks',
@@ -7,10 +9,32 @@ import { UsersService } from '../users.service';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {
-  userId = input.required<string>();
+export class UserTasksComponent implements OnInit{
+  // userId = input.required<string>();
+  userId = signal<string>("");
 
   private usersService = inject(UsersService);
+  private activatedRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
 
-  user = computed(() => this.usersService.users.find(u => u.id === this.userId()));
+  user?: User;
+  // user = computed(() => this.usersService.users.find(u => u.id === this.userId()));
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log(this.activatedRoute);
+    const subscription = this.activatedRoute
+    .paramMap
+    .subscribe({
+      next: (paramMap) => {
+        const userId = paramMap.get("userId");
+        if(userId) {
+          this.user = this.usersService.users.find((user) => user.id === userId);
+        }
+      }
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe())
+  }
 }
