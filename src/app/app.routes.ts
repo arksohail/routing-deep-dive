e43ce92fb3dfc8fefff8f802902439fd4901b230
arksohail <arksohail@gmail.com>
2024-10-03 01:46:24 +0530

    Introducing Route Guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,21 @@
-import { Routes } from "@angular/router";
+import { CanMatchFn, RedirectCommand, ResolveFn, Router, Routes } from "@angular/router";
 import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { resolveTitle, resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import { routes as userRoutes } from "./users/user.routes";
+import { inject } from "@angular/core";
+
+
+export const dummyCanmatch: CanMatchFn = (route, segments) => {
+  const router = inject(Router);
+  const shouldGetAccess = Math.random();
+  if(shouldGetAccess < 0.5) {
+    return true;
+  } else {
+    return new RedirectCommand(router.parseUrl('/unauthorized'));
+  }
+}
 
 export const routes: Routes = [
   {
@@ -19,6 +31,7 @@ export const routes: Routes = [
     path: 'user/:userId',
     component: UserTasksComponent,
     children: userRoutes,
+    canMatch: [dummyCanmatch],
     data: {
       message: "HELLO WORLD"
     },
