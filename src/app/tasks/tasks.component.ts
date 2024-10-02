3717939fc5d49ded9c21fe507ebd798af6b0d675
arksohail <arksohail@gmail.com>
2024-10-02 23:57:10 +0530

    Extracting Query Parameters via Observables

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,8 +1,8 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { TasksService } from './tasks.service';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
@@ -11,10 +11,21 @@ import { RouterLink } from '@angular/router';
   styleUrl: './tasks.component.css',
   imports: [TaskComponent, RouterLink],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
   userId = input.required<string>(); // Only accesible using the withRouterConfig({paramsInheritanceStrategy: 'always'})
-  order = input<'asc' | 'dsc'>();
+  order?: 'asc' | 'desc';
+  private activatedRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
   private taskService = inject(TasksService);
   userTasks = computed(() => this.taskService.allTasks().filter(u => u.userId === this.userId()));
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    const subscription = this.activatedRoute.queryParams.subscribe({
+      next: (params) => this.order = params['order']
+    });
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 
 }
